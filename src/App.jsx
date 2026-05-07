42480a286ac5fda96dd3e36551a2ed7e8f3f5c2a
shahid-codes-44 <shahidfirozaba@gmail.com>
2026-05-07 15:57:05 +0530

    Tower Of Hanoi

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from "./pages/Home.jsx";
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+      </Routes>
+    </>
+  )
+}
+
+export default App
