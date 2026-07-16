2b17af6b8dd93e86c907936060750648bdf5fc49
SHAHID-CODES-44 <shahid@teaminertia.com>
2026-07-16 17:40:39 +0530

    Code Setup for Upcoming Visualizers

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import { Route, Routes } from 'react-router-dom'
-import Home from "./pages/Home.jsx";
+import Home from './Pages/Home.jsx'
+import TowerOfHanoi from "./Visualizers/TowerOfHanoi.jsx";
+import BubbleSort from './Visualizers/BubbleSort.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +11,8 @@ function App() {
     <>
       <Routes>
         <Route path='/' element={<Home/>} />
+        <Route path='/TowerOfHanoi' element={<TowerOfHanoi/>} />
+        <Route path='/BubbleSort' element={<BubbleSort/>} />
       </Routes>
     </>
   )
---
+++ src/Visualizers/BubbleSort.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+const BubbleSort = () => {
+    return (
+        <>
+        <div className="main-container">
+            <h1>Bubble Sorting Visualizer Coming Soon......</h1>
+        </div>
+        </>
+    )
+}
+
+export default BubbleSort;
