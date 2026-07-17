c1c6d132d9a68e6d7f1e6aab2d8de4657c772e4b
SHAHID-CODES-44 <shahid@teaminertia.com>
2026-07-17 09:46:33 +0530

    Production Error Fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,18 @@
-
+import { Link } from "react-router-dom";
+import TowerOfHanoi from "../Visualizers/TowerOfHanoi";
+import BubbleSort from "../Visualizers/BubbleSort";
 
 const Home = () => {
     return (
-        <>
-        <a href="/TowerOfHanoi">TowerOfHanoi</a>
-        <br />
-        <a href="/BubbleSort">BubbleSort</a>
-        </>
-    )
-}
+        <div className="home-main-container">
+            <h1>Checkout Our Visualizers</h1>
+            <br />
+           <Link to="/TowerOfHanoi">Tower Of Hanoi</Link>
+           <br />
+           <br />
+           <Link to="/BubbleSort">Bubble Sort</Link>
+        </div>
+    );
+};
 
 export default Home;
